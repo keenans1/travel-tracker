5a1b0989ee36c4950a0c9acabdabf99eb53cd94c
Keenan Southall <keenan.southall@yahoo.com>
2023-01-11 13:37:09 -0700

    Create getUserTrips method

--- src/traveler.js
+++ src/traveler.js
@@ -1,6 +1,35 @@
 class Traveler {
-    constructor() {
+    constructor(id, name, travelerType) {
+        this.id = id;
+        this.name = name;
+        this.travelerType = travelerType;
+    }
 
+    getUserTrips(traveler, trips, destinations) {
+        const allTrips = trips.trips.reduce((acc, trip) => {
+            if (traveler.id === trip.userID) {
+                acc.push(trip)
+            }
+            return acc;
+        }, [])
+
+        // const allDestinations = allTrips.reduce((acc, trip) => {
+        //     destinations.destinations.forEach(destination => {
+        //         if (trip.destinationID === destination.id) {
+        //             acc.push(destination)
+        //         }
+        //     });
+        //     return acc
+        // }, [])
+
+        return allTrips.reduce((acc, trip) => {
+            destinations.destinations.forEach(destination => {
+                if (trip.destinationID === destination.id) {
+                    acc.push({ [destination.destination]: trip.date })
+                }
+            });
+            return acc
+        }, [])
     }
 }
 
