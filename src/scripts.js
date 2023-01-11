673b9aee649858a708b98a9bb0cad4f0977a43de
Keenan Southall <keenan.southall@yahoo.com>
2023-01-10 22:05:59 -0700

    Set up basic log in functionality

--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,24 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
+//import User from '../src/User'
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+//import './images/turing-logo.png'
 
 
-console.log('This is the JavaScript entry file - your code begins here.');
+const logInButton = document.getElementById('logInButton');
+const logInPage = document.querySelector('.logInPage');
+const mainPage = document.querySelector('.mainPage');
+const emailBox = document.getElementById('emailBox');
+const passwordBox = document.getElementById('passwordBox');
+
+logInButton.addEventListener('click', logIn)
+
+function logIn() {
+    if (emailBox.value === 'traveler' && passwordBox.value === 'travel') {
+        logInPage.classList.add('hidden')
+        mainPage.classList.remove('hidden')
+    } else {
+        alert('wrong')
+    }
+}
