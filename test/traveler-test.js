a466022ad4f9bbc16f8985801ca447e53c66707b
Keenan Southall <keenan.southall@yahoo.com>
2023-01-10 21:48:24 -0700

    Add an id to traveler

--- test/traveler-test.js
+++ test/traveler-test.js
@@ -3,7 +3,54 @@ const expect = chai.expect;
 import Traveler from '../src/traveler'
 
 describe('Traveler', function () {
+
+    const travelers = {
+        travelers: {
+            travelers: [
+                {
+                    id: 1,
+                    name: "Ham Leadbeater",
+                    travelerType: "relaxer"
+                },
+                {
+                    id: 2,
+                    name: "Rachael Vaughten",
+                    travelerType: "thrill-seeker"
+                },
+                {
+                    id: 3,
+                    name: "Sibby Dawidowitsch",
+                    travelerType: "shopper"
+                },
+                {
+                    id: 4,
+                    name: "Leila Thebeaud",
+                    travelerType: "photographer"
+                },
+                {
+                    id: 5,
+                    name: "Tiffy Grout",
+                    travelerType: "thrill-seeker"
+                }
+            ]
+        }
+    }
+
     it('should be a function', function () {
         expect(Traveler).to.be.a('function');
     });
+
+    it('should have an id', function () {
+        const traveler = new Traveler(5);
+        expect(traveler.id).to.equal(5);
+    });
+
+    it('should have a name', function () {
+        const traveler = new Traveler(5, 'John Doe');
+        expect(traveler.name).to.equal('ohn Doe');
+    });
+
+    // it('should have a traveler type', function () {
+    //     expect(Traveler).to.be.a('function');
+    // });
 });
