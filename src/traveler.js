f64612c2d257cafff1349769e6307972bee58f02
Keenan Southall <keenan.southall@yahoo.com>
2023-01-12 10:27:37 -0700

    Complete getTotalSpentForYear method

--- src/traveler.js
+++ src/traveler.js
@@ -5,31 +5,49 @@ class Traveler {
         this.travelerType = travelerType;
     }
 
-    getUserTrips(traveler, trips, destinations) {
-        const allTrips = trips.trips.reduce((acc, trip) => {
-            if (traveler.id === trip.userID) {
-                acc.push(trip)
-            }
-            return acc;
-        }, [])
-
-        // const allDestinations = allTrips.reduce((acc, trip) => {
-        //     destinations.destinations.forEach(destination => {
-        //         if (trip.destinationID === destination.id) {
-        //             acc.push(destination)
-        //         }
-        //     });
-        //     return acc
-        // }, [])
-
-        return allTrips.reduce((acc, trip) => {
-            destinations.destinations.forEach(destination => {
-                if (trip.destinationID === destination.id) {
-                    acc.push({ [destination.destination]: trip.date })
+    getTravelerTrips(traveler, trips, destinations) {
+        return trips.trips
+            .reduce((acc, trip) => {
+                if (traveler.id === trip.userID) {
+                    acc.push(trip)
                 }
-            });
-            return acc
-        }, [])
+                return acc;
+            }, [])
+            .reduce((acc, trip) => {
+                destinations.destinations.forEach(destination => {
+                    if (trip.destinationID === destination.id) {
+                        acc.push({ [destination.destination]: trip.date })
+                    }
+                });
+                return acc
+            }, [])
+    }
+
+    getTotalSpentForYear(traveler, trips, destinations, year) {
+        return trips.trips
+            .reduce((acc, trip) => {
+                if (traveler.id === trip.userID) {
+                    acc.push(trip)
+                }
+                return acc;
+            }, [])
+            .filter(trip => {
+                const dateSplit = trip.date.split('/')
+                if (dateSplit[0] === year) {
+                    return trip
+                }
+            })
+            .reduce((acc, trip) => {
+                const specificDestination = destinations.destinations.find(destination => trip.destinationID === destination.id)
+                if (specificDestination) {
+                    const flightCost = trip.travelers * specificDestination.estimatedFlightCostPerPerson;
+                    const hotelCost = trip.duration * specificDestination.estimatedLodgingCostPerDay;
+                    const beforeFee = flightCost + hotelCost;
+                    const afterFee = Number((beforeFee * 1.1).toFixed(0));
+                    acc += afterFee
+                }
+                return acc
+            }, 0)
     }
 }
 
